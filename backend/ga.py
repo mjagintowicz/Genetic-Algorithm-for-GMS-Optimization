91bd688ccf8fe8d043248398da8240ad23f63c45
Maria Jagintowicz <mjagintowicz1@gmail.com>
2026-07-06 20:35:18 +0200

    refactor and debug up to the selection point

--- backend/ga.py
+++ backend/ga.py
@@ -1,28 +1,16 @@
-from units import *
+from units import Unit, Schedule
+import random
 
 class Individual:
-    """
-    Individual -- a single schedule
-    """
-    def __init__(self, schedule):
-        self.schedule = schedule
-        self.fitness_cost = 0.0
-        self.fitness_reliability = 0.0
+    def __init__(self, schedule_list):
+        self.schedule = schedule_list
+        self.fitness = 0.0
 
-    def initialize_schedule(self, T, t, K):
-        """
-        :param T: time horizon
-        :param t: number of periods in T
-        :param K: number of units
-        A schedules will have randomly assigned unit indices (0; K); length T/t
-        """
-        tmp = []
-        self.schedule = tmp
+    def __repr__(self):
+        return f"IND: {self.schedule}\nFIT: {self.fitness}"
 
-    def calculate_fitness_cost(self, operation_coeff, cf):
-        """
-        Recalculates fitness of individual. Cost function.
-        """
+    def calculate_fitness_cost(self, operation_coef, cf):
+
         T = len(self.schedule)
         K = max(self.schedule)
         for k in range(K):  # for each unit analyze the schedules for calcs
@@ -31,14 +19,15 @@ class Individual:
             for t in range(T):
                 k_main = self.schedule[t]       # unit maintained in t
                 if k_main == k:                 # if there is maintenance add the working costs and reset, add main cost and remember when it was
-                    self.fitness_cost += operation_coeff * periods_worked + cf[t - last_maintenance_period]
+                    self.fitness += operation_coef * periods_worked + cf[t - last_maintenance_period]
                     periods_worked = 0
                     last_maintenance_period = t
                 else:   # if there is no maintenance of k, just keep working
                     periods_worked += 1
-            self.fitness_cost += operation_coeff * periods_worked   # if there is some unadded work in the end - add it now
+            self.fitness += operation_coef * periods_worked   # if there is some unadded work in the end - add it now
+            self.fitness = 1 / (self.fitness + 1) # transformed bc it should be minimized
 
-    def calculate_fitness_reliability(self, units, demands, t_coeff):
+    def calculate_fitness_reliability(self, units, demands):
 
         T = len(self.schedule)
         K = max(self.schedule)
@@ -47,63 +36,101 @@ class Individual:
             power_generated = 0
             for k in range(K):          # all units except this one worked - sum it's power
                 if k != k_main:
-                    power_generated += units[k].power * t_coeff
-            self.fitness_reliability += power_generated - demands[t]        # check if the demand was exceeded for the t
-
+                    power_generated += units[k].power
+            self.fitness += power_generated - demands[t]        # check if the demand was exceeded for the t
 
 
 class Population:
-    """
-    Population -- a collection of individuals (schedules)
-    """
+    def __init__(self, size, units, n_periods, operation_coef, cf, demands, criterion):
 
-    def __init__(self):
         self.individuals = []
 
-    def initialize_population(self, size, schedules, operation_coeff, cf, units, demands, t_coeff):
-        """
-        Creates size of random individuals (schedules)
-        """
         for i in range(size):
-            schedule = Individual(schedules[i])
-            schedule.calculate_fitness_cost(operation_coeff, cf)
-            schedule.calculate_fitness_reliability(units, demands, t_coeff)
-            self.individuals.append(schedule)
+            schedule = Schedule(units, n_periods)
+            individual = Individual(schedule.schedule)
+            if criterion == "Cost":
+                individual.calculate_fitness_cost(operation_coef, cf)
+            else:
+                individual.calculate_fitness_reliability(units, demands)
+            self.individuals.append(individual)
+
+    def __repr__(self):
+        s = ""
+        for individual in self.individuals:
+            s += f"{individual}\n"
+        return s
 
 
 class GeneticAlgorithm:
 
-    def __init__(self, population_size, generations, schedules, operation_coeff, cf, units, demands, t_coeff):
+    def __init__(self, population_size, units, n_periods, operation_coef, cf, demands, criterion, generations, selection_rate=0.6):
 
-        self.population = Population()
-        self.population.initialize_population(population_size, schedules, operation_coeff, cf, units, demands, t_coeff)
+        self.population = Population(population_size, units, n_periods, operation_coef, cf, demands, criterion)
         self.next_population = []
 
         self.generations = generations
+        self.population_size = population_size
+        self.selection_rate = selection_rate
 
-    def select_parents(self):
-        """
-        Chooses parents for crossover.
-        """
-        parent_A = self.population.schedules[0]
-        parent_B = self.population.schedules[1]
-        return parent_A, parent_B
+        self.best = None
 
-    def crossover(self, parent_A, parent_B):
+    def selection_roulette(self):
+
+        individuals = self.population.individuals
+        total_fitness = sum(ind.fitness for ind in individuals)
+
+        parents = []
+        n_parents = int(self.selection_rate * self.population_size)
+        for n in range(n_parents):
+
+            r = random.uniform(0, total_fitness)
+            current_sum = 0
+
+            for ind in individuals:
+                current_sum += ind.fitness
+
+                if current_sum >= r:
+                    parents.append(ind)
+                    break
+        return parents
+
+
+    def selection_tournament(self, tournament_size=3):
+        parents = []
+        individuals = self.population.individuals
+
+        n_parents = int(self.selection_rate * self.population_size)
+        for i in range(n_parents):
+            tournament = random.sample(individuals, tournament_size)
+            winner = max(tournament, key=lambda ind: ind.fitness)
+            parents.append(winner)
+
+        return parents
+
+
+    def selection(self, mode):
+
+        # Assume that all are picked for selection
+
+        if mode == "roulette":          # Dispatch the selected mode
+            return self.selection_roulette()
+        else:
+            return self.selection_tournament()
+
+
+    def crossover(self):
+        pass
+
+    def repair(self):
         """
-        Crossover between parents. Calculate fitness!
+        Repairs new individuals after crossover.
+        1. Finds unit indices missing from the schedule.
+        2. Replaces random empty period with the maintenance of the missing unit.
+        :return:
         """
-        child_A = parent_A
-        child_A.calculate_fitness()
-        child_B = parent_B
-        child_B.calculate_fitness()
-        self.next_population.append(child_A)
-        self.next_population.append(child_B)
+        pass
 
     def mutate(self):
-        """
-        Mutate an individual (schedule). Either a child or randomly selected. Calculate fitness!
-        """
         pass
 
     def recombine(self):
@@ -113,21 +140,11 @@ class GeneticAlgorithm:
         self.population = self.next_population
         self.next_population = []
 
-    def pick_solution(self):
-        """
-        Chooses the best individual from the population.
-        """
-        return self.population.schedules[0]
-
-
     def run(self):
         """
         Runs the main Genetic Algorithm loop.
         """
         for i in range(self.generations):
-            parent_A, parent_B = self.select_parents()
-            self.crossover(parent_A, parent_B)
-            self.mutate()
-            self.recombine()
+            continue
 
-        return self.pick_solution()
+        return self.best
--- backend/units.py
+++ backend/units.py
@@ -1,3 +1,5 @@
+import random
+
 class Period:
     def __init__(self, demand, unit_maintained):
         self.demand = demand
@@ -7,37 +9,22 @@ class Unit:
 
     def __init__(self, power, idx):
         self.power = power
-        self.periods_from_last_maintenance = 0
         self.idx = idx
 
         self.available = True
 
-    def operation_cost(self):
-        return self.periods_from_last_maintenance * self.operation_coef
-
     def is_operation_possible(self):
         return self.available
-
-    def operate(self):
-        if not self.available:
-            self.available = True
-        self.periods_from_last_maintenance += 1
-
-    def maintenance_cost(self, cf):
-        return cf[self.periods_from_last_maintenance]
 
     def is_maintenance_needed(self):
         return not self.available
 
-    def maintain(self):
-        if self.available:
-            self.available = False
-        self.periods_from_last_maintenance = 0
-
 
 class Schedule:
-    def __init__(self):
-        self.schedule = []
 
-    def append_unit(self, unit):
-        self.schedule.append(unit.idx)
+    def __init__(self, units, n_periods):
+
+        rng = random.Random()
+        self.schedule = [u.idx for u in units]
+        self.schedule.extend([0] * (n_periods - len(units)))
+        rng.shuffle(self.schedule)
